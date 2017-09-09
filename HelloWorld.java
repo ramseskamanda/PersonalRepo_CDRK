1a8cc76290761e29f747564ff0572f09b9faf22d
RKasinCDRK <31807135+RKasinCDRK@users.noreply.github.com>
2017-09-10 00:09:15 +0200

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld{
-	public static void main(String[] args) {
-		String ascii_art = ""
-        String invalidCharReplaced = ascii_art.replaceAll("[\\\\/:*?\"<>|]", "#");
-		System.out.print(invalidCharReplaced);
-	}
-}
