41ef5ac667cef66ae749ed8d02f4685c295a2806
RKasinCDRK <ramses.kamanda@gmail.com>
2017-09-15 15:29:57 +0200

    "New command file"

---
+++ GameDevelopment/MarioEnv/Commands.py
@@ -0,0 +1,15 @@
+from pygame.locals import *
+
+# Check what keys are being pressed
+# And call associated functions.
+def input_handler(keys):
+    if keys[K_q]:
+        return False
+    if keys[K_LEFT]:
+        pass
+    elif keys[K_RIGHT]:
+        pass
+    if keys[K_UP]:
+        pass
+    elif keys[K_DOWN]:
+        pass
