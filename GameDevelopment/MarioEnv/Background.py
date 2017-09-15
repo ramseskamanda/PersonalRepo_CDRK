5dc25e7409550b798ed3abf5bf46c4a1e7164064
CDasinCDRK <31819539+CDasinCDRK@users.noreply.github.com>
2017-09-15 12:41:14 +0200

    Update Background.py

--- GameDevelopment/MarioEnv/Background.py
+++ GameDevelopment/MarioEnv/Background.py
@@ -1,18 +1,31 @@
 import pygame
 import os
-from PIL import Image
-from resizeimage import resizeimage
-from Constants import SCREEN_HEIGHT
+from Constants import RATIO, BACKGROUND_HEIGHT, BACKGROUND_WIDTH, TILE_SIZE
 
-fd_img = open('/Users/cdalenbrook/Documents/Mario/Images/background0.png', 'r')
-img = Image.open(fd_img)
-img = resizeimage.resize_height(img, SCREEN_HEIGHT)
-img.save('background1.png', img.format)
-fd_img.close()
-
-class Background_config:
+class Sprite_config:
     def __init__(self):
-        path = os.path.join('/Users/cdalenbrook/Documents/Mario/MarioEnv', '/Users/cdalenbrook/Documents/Mario/Images')
-        self.background_color = pygame.image.load(path + '/background1.png').convert()
+        self.path = os.path.join('/Users/cdalenbrook/Documents/Mario/MarioEnv', '/Users/cdalenbrook/Documents/Mario/Images')
+        self.background_config()
+        self.ground_config()
 
 
+    def background_config(self):
+        #loading the background color (blue rectangle)
+        self.background_color = pygame.image.load(self.path + '/background0.png').convert()
+        x, y = self.background_color.get_size()
+        self.background_color = pygame.transform.scale(self.background_color, (int(x*RATIO), int(y*RATIO)))
+
+    def ground_config(self):
+        self.ground_img = pygame.image.load(self.path + '/Ground.png').convert()
+        self.ground_img = pygame.transform.scale(self.ground_img, (TILE_SIZE, TILE_SIZE))
+        self.ground = []
+        for y in range(BACKGROUND_HEIGHT-2*TILE_SIZE, BACKGROUND_HEIGHT, TILE_SIZE):
+            for x in range(0, BACKGROUND_WIDTH, TILE_SIZE):
+                self.ground.append((x, y))
+
+    def show(self, img, screen, array=None, coords=None):
+        if array is not None:
+            for i in array:
+                screen.blit(img, i)
+        else:
+            screen.blit(img, coords)
