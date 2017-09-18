f25ffcb5cb160d95319f779c812d7459baf4e4b6
RKasinCDRK <ramses.kamanda@gmail.com>
2017-09-18 17:45:26 +0200

    More Changes

--- GameDevelopment/MarioEnv/Sprites_.py
+++ GameDevelopment/MarioEnv/Sprites_.py
@@ -2,34 +2,50 @@ import pygame
 from Constants import *
 from Images import clouds, pipes, bricks, coin, peach
 
+def Sprite_init():
+    p = 0
+    wc = 0
+    b = 0
+    c = 0
+    ALL_SPRITES = pygame.sprite.Group(p, wc, b, c, Peach())
+
 
 class Pipes(pygame.sprite.Sprite):
-    def __init__(self, size):
+    def __init__(self, size, position):
         super().__init__()
         self.pipes = pipes()
         self.image = self.pipes[size]
         self.rect = self.image.get_rect()
+        self.rect.x = position(0)
+        self.rect.y = position(1)
 
 class WalkableClouds(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, position):
         super().__init__()
         self.clouds = clouds()
         self.image = self.clouds['img']
         self.rect = self.image.get_rect()
+        self.rect.x = position(0)
+        self.rect.y = position(1)
+
 
 class Bricks(pygame.sprite.Sprite):
-    def __init__(self, counter, x, y):
+    def __init__(self, counter, position):
         super().__init__()
         self.bricks = bricks()
         self.image = self.bricks[counter]
         self.rect = self.image.get_rect()
+        self.rect.x = position(0)
+        self.rect.y = position(1)
 
 class Coin(pygame.sprite.Sprite):
-    def __init__(self, counter, x, y):
+    def __init__(self, counter, position):
         super().__init__()
         self.coin = coin()
         self.image = self.coin[0]
         self.rect = self.image.get_rect()
+        self.rect.x = position(0)
+        self.rect.y = position(1)
 
 class Peach(pygame.sprite.Sprite):
     def __init__(self):
@@ -37,3 +53,4 @@ class Peach(pygame.sprite.Sprite):
         self.peach = peach()
         self.image = self.peach[0]
         self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = PEACH_POS
